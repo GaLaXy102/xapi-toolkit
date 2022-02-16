1c1a6e41d158eeabe240c9918637ecd8fe67c83d
Konstantin Köhring <konstantin.koehring@tu-dresden.de>
2022-02-16 01:54:13 +0100

    Show 404 for unknown routes (Closes #52)

--- src/main/java/de/tudresden/inf/verdatas/xapitools/RootRedirectController.java
+++ src/main/java/de/tudresden/inf/verdatas/xapitools/RootRedirectController.java
@@ -1,9 +1,15 @@
 package de.tudresden.inf.verdatas.xapitools;
 
 import de.tudresden.inf.verdatas.xapitools.ui.BasepageMavController;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 /**
  * This Controller only adds a Redirect for the Root-Context ("/")
@@ -11,7 +17,7 @@ import org.springframework.web.servlet.view.RedirectView;
  * @author Konstantin Köhring (@Galaxy102)
  */
 @Controller
-public class RootRedirectController {
+public class RootRedirectController implements ErrorController {
 
     /**
      * Simple 302 Redirect to {@link BasepageMavController#showHome()}
@@ -19,5 +25,10 @@ public class RootRedirectController {
     @GetMapping("/")
     public RedirectView redirectToUi() {
         return new RedirectView("/ui");
+    }
+
+    @GetMapping("/error")
+    public ModelAndView showNotFound() {
+        throw new NoSuchElementException("No such page");
     }
 }
